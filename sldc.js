d55e778c4471d55156442adec4ec7a79f0548fe1
Blake Pond <blakehp@icloud.com>
2021-10-15 10:29:15 -0600

    initail commit

--- sldc.js
+++ sldc.js
@@ -6,14 +6,22 @@
 */
 
 // Analysis of Requirements
+// Planning
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
 /*
-    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    Create a variable for each of the above steps of the Software Development Lifecycle 
+    and set the values of each variable to a definition of the step.
 */
+
+let analysisOfRequirements = "This is where we come up with the intial idea or solution to a problem. We gather and clarify business requirements and potential risks and address them before moving to planning stage."
+let planning = "This is where we outline the idea of solution to a problem. This is where we assign tasks and rescources required to build idea or solve problem. 
+let design = "This is where we design in detail the features in the product or the steps in order to solve the problem. Database specification is designed for data management, storage and future processing, retrieval and evaluation."
+let implementation = "This is where we start taking the idea or problem and code it into a tangible solution."
+let testingAndIntegration = "This is where we test and monitor for bugs and defects. Once issues have been fixed we deploy the product."
+let maintain = "This is where we maintain the product by implementing upgrades, enhancements and nessesary changes"
